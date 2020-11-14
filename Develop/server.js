45095e3da0a990852ebb4f59e12ee5210e390b97
katwithaC <catherinerosearredondo@gmail.com>
2020-11-13 23:10:09 -0600

    adding heroku and mongodb connections

--- Develop/server.js
+++ Develop/server.js
@@ -3,7 +3,8 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+// heroku connection
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,10 +16,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+//MongoDB connection
+mongoose.connect("mongodb://localhost/budget", 
+{
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
-});
+}
+);
 
 // routes
 app.use(require("./routes/api.js"));
