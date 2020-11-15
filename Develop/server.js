29c481387cafac06ec52b63293c0cbc0b347a906
katwithaC <catherinerosearredondo@gmail.com>
2020-11-14 21:08:32 -0600

    updating service-worker.js

--- Develop/server.js
+++ Develop/server.js
@@ -3,6 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
+
 // heroku connection
 const PORT = process.env.PORT || 3000;
 
